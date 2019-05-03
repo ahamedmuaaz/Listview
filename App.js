f3e10b4aa7080828e681d8783cdee2f9cd43d2ad
AhamedMuaaz <mma.muaaz@gmail.com>
2019-05-03 15:01:31 +0530

    initialization

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View,AsyncStorage} from 'react-native';
+
+
+
+class Welcome extends React.Component {
+
+  componentDidMount(){
+    
+     fetch('http://35.246.54.179/login/', {
+     method: 'POST',
+     headers: {
+     Accept: 'application/json',
+    'Content-Type': 'application/json',
+     },
+     body: JSON.stringify({
+    'email': 'sahandilshan222@gmail.com',
+    'pass': 'test1234'
+     }),
+
+    }).then((response) => response.json())
+    .then((responseJson) => {
+    console.log(responseJson);
+ })
+ .catch((error) => {
+   console.error(error);
+ });
+  //this._storeData();
+  this._retrieveData();
+    //console.log("moubnted");
+  }
+  render() {
+    return( <Text>Hello</Text>);
+
+  }
+
+  _storeData = async () => {
+    try {
+      await AsyncStorage.setItem('@MySuperStore', 'I like to save it.');
+    } catch (error) {
+      // Error saving data
+      console.log(error);
+    }
+  };
+
+  _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@MySuperStore');
+      if (value !== null) {
+        // We have data!!
+        console.log(value);
+      }
+    } catch (error) {
+      // Error retrieving data
+      console.log(error);
+    }
+  };
+}
+
+export default Welcome;
